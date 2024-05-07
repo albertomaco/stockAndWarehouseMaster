312bfb1253eff18bd88a8c2375f010b91bc82de5
albertoblazquez <Alberto.Blazquez@usal.es>
2024-05-07 19:21:02 +0200

    primer commit codigo backend completo

--- stockAndWarehouseMaster/src/main/java/com/tfgAlberto/stockAndWarehouseMaster/StockAndWarehouseMasterApplication.java
+++ stockAndWarehouseMaster/src/main/java/com/tfgAlberto/stockAndWarehouseMaster/StockAndWarehouseMasterApplication.java
@@ -1,14 +1,21 @@
 package com.tfgAlberto.stockAndWarehouseMaster;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class StockAndWarehouseMasterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockAndWarehouseMasterApplication.class, args);
 	}
 
+	@Bean
+    public ScheduledExecutorService scheduledExecutorService() {
+        return Executors.newScheduledThreadPool(15);
+    }
 }
---
+++ stockAndWarehouseMaster/src/main/java/com/tfgAlberto/stockAndWarehouseMaster/almacen/controller/AlmacenController.java
@@ -0,0 +1,70 @@
+package com.tfgAlberto.stockAndWarehouseMaster.almacen.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tfgAlberto.stockAndWarehouseMaster.almacen.model.Almacen;
+import com.tfgAlberto.stockAndWarehouseMaster.almacen.service.AlmacenService;
+import com.tfgAlberto.stockAndWarehouseMaster.payload.response.MessageResponse;
+import com.tfgAlberto.stockAndWarehouseMaster.util.Utilidades;
+
+@RestController()
+@RequestMapping("/api/almacen")
+public class AlmacenController {
+
+	@Autowired
+	private AlmacenService almacenService;
+
+	@Autowired
+    public AlmacenController(AlmacenService almacenService) {
+        this.almacenService = almacenService;
+    }
+
+	@PostMapping("/getDesplegables")
+    public ResponseEntity<List<Utilidades>> getDesplegables(@RequestBody Integer id) {
+		List<Utilidades> desplegable = almacenService.find(id);
+		return new ResponseEntity<>(desplegable, HttpStatus.OK);
+    }
+
+	@PostMapping("/newAlmacen")
+    public ResponseEntity<?> insertAlmacen(@RequestBody Almacen almacen) {
+		if(null != almacen) {
+			if(null != almacen.getId()) {
+				almacenService.updateAlmacen(almacen);
+				return new ResponseEntity<>(new MessageResponse("Almacén actualizado correctamente"), HttpStatus.OK);
+			}
+			almacenService.insertAlmacen(almacen);
+		}
+		return new ResponseEntity<>(new MessageResponse("Hubo algún problema, consulte con el administrador"), HttpStatus.BAD_REQUEST);
+    }
+
+	@GetMapping("/listaAlmacenes")
+	public ResponseEntity<List<Almacen>> getAllAlmacenes() {
+		List<Almacen> listaProductos = almacenService.findAll();
+		return new ResponseEntity<List<Almacen>>(listaProductos, HttpStatus.OK);
+    }
+	
+	@GetMapping("detail/{id}")
+	public ResponseEntity<Almacen> getOneById(@RequestParam("id") long id) {
+		Almacen almacen = almacenService.findById(id).get();
+		return new ResponseEntity<Almacen>(almacen, HttpStatus.OK);
+    }
+
+	/*@PostMapping("/deleteAlmacen")
+    public ResponseEntity<?> deleteAlmacen(@RequestBody Almacen almacen) {
+		if(null != almacen) {
+			almacenService.deleteAlmacen(almacen);
+		}
+		return new ResponseEntity(HttpStatus.OK);
+    }*/
+	
+}
